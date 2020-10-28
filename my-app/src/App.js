844c1154fad6aa7dc16dc29cd0bfdea0b3edf474
John Doolittle <jdoo.code@gmail.com>
2020-10-28 16:28:52 -0600

    Created counter

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>
+            Click me
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
