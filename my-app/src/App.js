7dbaa6225e4ec4af0bf237b9262f389b57b2f5bc
John Doolittle <jdoo.code@gmail.com>
2020-10-28 18:07:35 -0600

    Inserted React Logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 import CountBtn from './CountBtn.jsx';
+import logo from './logo192.png';
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Logo" />
         <div>
           <p>You clicked {count} times</p>
           <CountBtn
